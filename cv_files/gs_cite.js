2eaf2acefd9c94234cae7c1eddb5e00cd386212e
Fan XUE <fanxue@outlook.com>
2026-03-26 12:25:18 +0000

    remote update @ Cambridge

--- cv_files/gs_cite.js
+++ cv_files/gs_cite.js
@@ -1,3 +1,3 @@
-var gs_cites=[{"year": 2016, "value": 23}, {"year": 2017, "value": 93}, {"year": 2018, "value": 212}, {"year": 2019, "value": 393}, {"year": 2020, "value": 528}, {"year": 2021, "value": 856}, {"year": 2022, "value": 1397}, {"year": 2023, "value": 1573}, {"year": 2024, "value": 1703}, {"year": 2025, "value": 1727}, {"year": 2026, "value": 214}];
+var gs_cites=[{"year": 2016, "value": 23}, {"year": 2017, "value": 93}, {"year": 2018, "value": 212}, {"year": 2019, "value": 393}, {"year": 2020, "value": 528}, {"year": 2021, "value": 856}, {"year": 2022, "value": 1397}, {"year": 2023, "value": 1568}, {"year": 2024, "value": 1657}, {"year": 2025, "value": 1777}, {"year": 2026, "value": 393}];
 var hindex=50;
-var tot_cites=8805;
+var tot_cites=8982;

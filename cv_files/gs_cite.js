feeb26946ebecc0b60637014f3a99c6ae0baa9ba
Fan XUE <fanxue@outlook.com>
2026-09-03 09:31:12 +0800

    Update gs_cite.js

--- cv_files/gs_cite.js
+++ cv_files/gs_cite.js
@@ -1,3 +1,3 @@
-var gs_cites=[{"year": 2017, "value": 96}, {"year": 2018, "value": 213}, {"year": 2019, "value": 400}, {"year": 2020, "value": 526}, {"year": 2021, "value": 850}, {"year": 2022, "value": 1389}, {"year": 2023, "value": 1583}, {"year": 2024, "value": 1654}, {"year": 2025, "value": 1752}, {"year": 2026, "value": 1105}];
+var gs_cites=[{"year": 2017, "value": 96}, {"year": 2018, "value": 213}, {"year": 2019, "value": 400}, {"year": 2020, "value": 527}, {"year": 2021, "value": 855}, {"year": 2022, "value": 1387}, {"year": 2023, "value": 1584}, {"year": 2024, "value": 1655}, {"year": 2025, "value": 1752}, {"year": 2026, "value": 1116}];
 var hindex=52;
-var tot_cites=9707;
+var tot_cites=9726;
